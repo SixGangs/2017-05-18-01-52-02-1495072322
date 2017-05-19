91fe71912baaf286b6541b6e7cb50d36558ee4ea
SixGangs <2577944135@qq.com>
2017-05-19 15:53:46 +0800

    一次性全部提交

--- main/student.js
+++ main/student.js
@@ -1,3 +1,10 @@
 let Person = require('./person')
 // Write your code here
 module.exports = Student
+function Student(name,age,klass){
+		Person.call(this);
+		this.klass = klass;
+		this.introduce = function(){
+		return 'My name is '+name+'. I am '+age+' years old. I am a Student. I am at Class '+klass+'.';
+	}
+}
